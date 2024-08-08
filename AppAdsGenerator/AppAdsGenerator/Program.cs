04dc0bfd080a28dd0e45ee7fed15fea876c4702b
Tapan <tapan@spill.games>
2024-08-08 12:14:39 +0530

    AppAds generator

---
+++ AppAdsGenerator/AppAdsGenerator/Program.cs
@@ -0,0 +1,27 @@
+﻿var directoryPath = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName;
+var dataSourcesPath = Path.Combine(directoryPath, "DataSources");
+var dataSourcesTextFiles = Directory.GetFiles(dataSourcesPath);
+
+var result = new List<string>();
+foreach (var file in dataSourcesTextFiles)
+{
+    var textsFromFile = File.ReadLines(file);
+    foreach (var text in textsFromFile)
+    {
+        if (string.IsNullOrEmpty(text) || string.IsNullOrWhiteSpace(text))
+        {
+            continue;
+        }
+        if (text.StartsWith('#'))
+        {
+            continue;
+        }
+        result.Add(text.Trim().Replace(", ", ","));
+    }
+}
+result.Sort();
+result = result.ToHashSet().ToList();
+var output = string.Join("\n", result);
+var outputFileParent = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.Parent;
+var outputPath = Path.Combine(outputFileParent.FullName, "output.txt");
+File.WriteAllText(outputPath, output);
